4753243ec315d6943dc718d4afff60d55b309a3b
Sain Saburaj <sainsaburajpnc@gmail.com>
2025-05-07 22:16:35 +0530

    Partially finished index page

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#4D0000',
+                secondary: '#A20000',
+            }
+        }
+    }
+}
